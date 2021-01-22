2156447afab9bbc9eb925e5ff0fad6b6b2a3ed58
amankkg <amankubanychbek@gmail.com>
2021-01-22 20:49:23 +0600

    add scaffolded identity pages

---
+++ WithouIdentity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WithouIdentity.Data;
+
+[assembly: HostingStartup(typeof(WithouIdentity.Areas.Identity.IdentityHostingStartup))]
+namespace WithouIdentity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AppIdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AppIdentityContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AppIdentityContext>();
+            });
+        }
+    }
+}
